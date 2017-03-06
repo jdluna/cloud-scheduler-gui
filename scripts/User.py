e634e3fd61ab7f27378e2e4d1af506dae4422952
Nannapas <bamboojfc@gmail.com>
2017-03-07 03:25:34 +0700

    set timezone complete
    - edit constructor of User to support when have only sessionId

--- scripts/User.py
+++ scripts/User.py
@@ -29,7 +29,7 @@ class User:
     __language = None
     
     
-    def __init__(self,username,userId,firstname,lastName,emailAddress,phoneNumber,status,organization,position,language,timezone): 
+    def __init__(self,username= None,userId= None,firstname= None,lastName= None,emailAddress= None,phoneNumber= None,status= None,organization= None,position= None,language= None,timezone= None,sessionId = None): 
         self.__username = username      
         self.__sessionId = None
         self.__userId = userId
@@ -42,8 +42,26 @@ class User:
         self.__position = position
         self.__language = language
         self.__timezone = timezone
-        self.__setSessionToken()
-
+        self.__db = Database()
+        
+        if self.__db.connect():
+        
+            if sessionId == None:
+                self.__setSessionToken()
+            else:
+                self.__sessionId = sessionId
+                
+            if self.__username == None:
+                #check session id and get user id
+                sql = 'SELECT `user_id` FROM `session` WHERE `session_id` = "'+str(self.__sessionId)+'";'
+               
+                self.__db.execute(sql)
+                uid = self.__db.getCursor().fetchone()
+                if uid != None:
+                    self.__userId = uid[0]
+                
+    def getUserId(self):
+        return self.__userId
         
     def getUsername(self):
         return self.__username
@@ -78,28 +96,33 @@ class User:
     def setTimezone(self, timezone):
         self.__timezone = timezone
         
+        if self.__userId != None:
+            sql = "UPDATE `user` SET `timezone`='"+str(self.__timezone)+"' WHERE `user_id`= '"+str(self.__userId)+"';"     
+            if self.__db.execute(sql):
+                self.__db.commit()
+                    
+
+        
     def __setSessionToken(self):
         self.__sessionId = self.__idGenerator()
-        
-        db = Database()
-        if db.connect():      
-            sql = "UPDATE `session` SET `session_id`='"+str(self.__sessionId)+"' WHERE `user_id`= '"+str(self.__userId)+"';"     
-            if db.execute(sql):
-                #this userId had ever logged in before.
-                db.commit()
-            else:
-                #no entry of this userId. (login first time)
-                while True:
-                    sql = "INSERT INTO `session`(`user_id`, `session_id`) VALUES ('"+str(self.__userId)+"','"+str(self.__sessionId)+"');"
-                
-                    if db.execute(sql):
-                        #session_id is not duplicated
-                        db.commit()
-                        break
-                    else:
-                        #session_id is duplicated
-                        self.__sessionId = self.__idGenerator()        
-            db.close()
+    
+        sql = "UPDATE `session` SET `session_id`='"+str(self.__sessionId)+"' WHERE `user_id`= '"+str(self.__userId)+"';"     
+        if self.__db.execute(sql):
+            #this userId had ever logged in before.
+            self.__db.commit()
+        else:
+            #no entry of this userId. (login first time)
+            while True:
+                sql = "INSERT INTO `session`(`user_id`, `session_id`) VALUES ('"+str(self.__userId)+"','"+str(self.__sessionId)+"');"
+            
+                if self.__db.execute(sql):
+                    #session_id is not duplicated
+                    self.__db.commit()
+                    break
+                else:
+                    #session_id is duplicated
+                    self.__sessionId = self.__idGenerator()        
+
         
         
     def getSessionToken(self):   
