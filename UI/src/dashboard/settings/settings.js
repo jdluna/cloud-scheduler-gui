ee0ffcabbe20e6084de2fd6adf451810f3ac5f5b
prapansak <prapansak_kaew@hotmail.com>
2017-05-14 15:05:18 +0700

    updated timezone GUI

--- UI/src/dashboard/settings/settings.js
+++ UI/src/dashboard/settings/settings.js
@@ -7,17 +7,20 @@ export default class Settings extends Component {
         return (
             <section className='modal'>
                     <section className={Style.panel}>
-                        <header><div>Settings</div></header>
+                        <header>
+                            <div>Settings</div>
+                            <img src='img/ic_close.svg' onClick={()=>this.props.settingContainer.onClose()} />
+                        </header>
                         <nav>
                             <div className={Style.menu}>
                                 <div>
                                     <img src='img/ic_access_time_white.svg'/>
-                                    <div className={Style.text}>Time zone</div>
+                                    <div className={Style.text}>Timezone</div>
                                 </div>
                             </div>
                         </nav>
                         <div className={Style.content}>
-                            <TimezoneContainer dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
+                            <TimezoneContainer settingContainer={this.props.settingContainer} dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
                         </div>
                     </section>
             </section>
--- UI/src/dashboard/settings/settingsContainer.js
+++ UI/src/dashboard/settings/settingsContainer.js
@@ -2,10 +2,28 @@ import React,{Component} from 'react'
 import Settings from './settings'
 
 export default class SettingsContainer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            timezoneTimer: null
+        }
+    }
+
+    setTimezoneThick(obj){
+        this.setState({
+            timezoneTimer: obj
+        })
+    }
+
+    onClose(){
+        clearInterval(this.state.timezoneTimer)
+        this.props.dashBoardContainer.onCloseModal()
+    }
+
     render() {
         return (
             <section>
-                <Settings dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
+                <Settings settingContainer={this} dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
             </section>
         )
     }
