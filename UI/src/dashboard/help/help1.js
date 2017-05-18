fa199e2675e292edce9a464fccc8a81703fb0649
prapansak <prapansak_kaew@hotmail.com>
2017-05-18 17:12:30 +0700

    updated tutorial image size

--- UI/src/dashboard/help/help1.js
+++ UI/src/dashboard/help/help1.js
@@ -7,7 +7,7 @@ export default class Help1 extends Component {
             <section className={Style.cardwrap}>
                 <div className={Style.title}>PRAGMA Cloud Testbed services</div>
                 <div className={Style.content}>
-                    <img width='85%' height='100%' src='img/tutorial-1.png'/>
+                    <img width='75%' height='100%' src='img/tutorial-1.png'/>
                 </div>
                 <div className={Style.detail}>
                     <div>This system visualizes all sitets on the map</div>
