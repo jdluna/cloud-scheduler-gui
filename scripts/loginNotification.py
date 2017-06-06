7ad4b889200462c754dbade7a0323d3b6e0be6a0
Nannapas <bamboojfc@gmail.com>
2017-06-06 14:34:36 +0700

    add_loginNotification

---
+++ scripts/loginNotification.py
@@ -0,0 +1,32 @@
+#!/opt/python/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 01 22:15:55 2017
+
+@author: bamboojfc
+"""
+
+import cgitb
+cgitb.enable()
+
+import cgi
+form = cgi.FieldStorage()
+
+###variable from front-end###
+SESSION_ID = form.getvalue('session_id')
+#############################
+
+print "Content-Type: text/html"     
+print "Access-Control-Allow-Origin: *"  
+print
+
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='348529',
+  key='43cff02ec435f9ddffa1',
+  secret='7f7f0f08b4b6a35aa509',
+  ssl=True
+)
+
+pusher_client.trigger('notification', 'login', {'message': SESSION_ID})
