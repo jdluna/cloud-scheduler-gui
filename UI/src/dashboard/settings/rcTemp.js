47d81234c762d66881992aecb18f120a6a8c0ab5
eunjeong Jang <cs801458j@gmail.com>
2018-02-23 13:20:42 -0800

    dont remove anything -- to myself

--- UI/src/dashboard/settings/rcTemp.js
+++ UI/src/dashboard/settings/rcTemp.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 import axios from 'axios'
 import moment from 'moment'
 import Reservation from '../reservation/reservation'
-import {SET_TIMEZONE_ENDPOINT,CONFIRM_RESOURCE_RESERVATION_ENDPOINT} from '../../config/endpoints'
+import {SET_TIMEZONE_ENDPOINT,CONFIRM_RESERVATION_ENDPOINT} from '../../config/endpoints'
 import { RESOURCES } from '../../config/attributes'
 import SuccessDialog from '../reservation/successDialog'
 import ErrorDialog from '../reservation/ErrorDialog'
@@ -16,15 +16,19 @@ export default class ResourceContainer extends Component{
 
 		 this.sites = this.props.sites
 
+        let resource = []
+        let siteInputDom = []
+
 	
-		this.state = {value:'', _name:'', description:'Virtual machines', contact:'', location:'', pragma_boot_path:'/opt/pragma_boot', pragma_boot_version:'1', python_path:'/opt/python/bin/python', temp_dir:'/var/run/pcc', username:'root', deployment_type:'Rocks KVM', site_hostname:'', latitude:'36.060839', longitude:'140.137303', total_cpu:'32', total_memory:'64'};
+		this.state = { _name:'', description:'Virtual machines', contact:'', location:'', pragma_boot_path:'/opt/pragma_boot', pragma_boot_version:'1', python_path:'/opt/python/bin/python', temp_dir:'/var/run/pcc', username:'root', deployment_type:'Rocks KVM', site_hostname:'', latitude:'36.060839', longitude:'140.137303', total_cpu:'32', total_memory:'64'};
 	
 	this.handleChange = this.handleChange.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
 	this.handleInputChange = this.handleInputChange.bind(this);
 	
-	this.onSubmit = this.onSubmit.bind(this);
+	this.onSubmitFormValues = this.onSubmitFormValues.bind(this);
 	this.queryConfirmReservation = this.queryConfirmReservation.bind(this);
+
 	this.onCloseSettings = this.onCloseSettings.bind(this)
 	}
 
@@ -50,14 +54,20 @@ export default class ResourceContainer extends Component{
 	}
 	
 
-	onSubmit(event){
-		console.log('mysubmit');
-	     let { _name, description, contact, location, pragma_boot_path, pragma_boot_version, python_path, temp_dir, username, latitude, longitude, total_cpu, total_memory} = this.state;
-            
-		alert(this.state._name);
+	onSubmitFormValues(event){
+	     let submitForm = event.target.name;
 
+	     let { _name, description, contact, location, pragma_boot_path, pragma_boot_version, python_path, temp_dir, username, latitude, longitude, total_cpu, total_memory} = this.state
+             this.queryConfirmReservation();
+	}
 
-	   let params = {
+	queryConfirmReservation(){
+	     let sitesId = '';
+             let resources = '';
+	     
+	    if( onContactChange() ){
+
+	     let params = {
             params:{
                 _name: this.state._name,
                 description: this.state.description,
@@ -82,50 +92,6 @@ export default class ResourceContainer extends Component{
             let {data,status} = response
             if(status==200&&data.result){
                 if(data.result=='success'){
-                    alert('success')
-                }else{
-                    
-                }
-            }
-        }).catch(error=>{
-            console.log('QUERY CONFIRM RESERVATION ERROR: '+error)
-        });
-	
-		event.preventDefault();
-	}
-
-
-	queryConfirmReservation(event){
-        let sitesId = 'root';
-        let resources = '';
-
-
-        let params = {
-            params:{
-                _name: this.state._name,
-                description: this.state.description,
-                contact: this.state.contact,
-                location: this.state.location,
-                pragma_boot_path: this.state.pragma_boot_path,
-                pragma_boot_version: this.state.pragma_boot_version,
-                python_path: this.state.python_path,
-                temp_dir: this.state.temp_dir,
-                username: this.state.username,
-                deployment_type: this.state.deployment_type,
-                site_hostname: this.state.site_hostname,
-                latitude: this.state.latitude,
-                longitude: this.state.longitude,
-                total_cpu: this.state.total_cpu,
-                total_memory: this.state.total_memory,
-                sites_id: sitesId
-            }
-        }
-
-
-        axios.get(CONFIRM_RESOURCE_RESERVATION_ENDPOINT,params).then(response=>{
-            let {data,status} = response
-            if(status==200&&data.result){
-                if(data.result=='success'){
                     this.changeDialog('success')
                     this.dashboardContainer.closeAllCard()
                 }else{
@@ -136,8 +102,21 @@ export default class ResourceContainer extends Component{
             console.log('QUERY CONFIRM RESERVATION ERROR: '+error)
         });
 
-    }
+	}else{
+		alert('fail');
+	}
+	}
 
+	onContactChange(){
+        let value = this.state.contact
+        let REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (value.match(REGEX)) {
+		return true;
+        } else {
+            return false;
+        }
+    }
+	
 
 	render(){
 		return (
--- UI/src/dashboard/settings/resource.js
+++ UI/src/dashboard/settings/resource.js
@@ -11,10 +11,8 @@ export default class Resource extends Component {
 					     <div className={Style.right}>
 						<div className={Style.width}>
 						     <div className={Style.time}>Resource form</div>
-						     <form onSubmit={this.handleSubmit}>
-							<lable className={Style.tag}>site_id:<input type='text' name='site_id' value={this.props.resourcesContainer.state.site_id} onChange={this.props.resourcesContainer.handleInputChange}/></lable>
-						     
-							<label>name:<input type='text' name='_name'value={this.props.resourcesContainer.state._name} onChange={this.props.resourcesContainer.handleInputChange} /></label>
+						     <form onSubmit={this.handleSubmit}> 
+							<label>name:<input type='text' name='_name' value={this.props.resourcesContainer.state._name} onChange={this.props.resourcesContainer.handleInputChange} /></label>
 						    
 							<label>description:<input type='text' name='description' value={this.props.resourcesContainer.state.description} onChange={this.props.resourcesContainer.handleInputChange}/></label>
 							
@@ -43,7 +41,8 @@ export default class Resource extends Component {
                                             		<label>total_cpu<input type='number' name='total_cpu' value={this.props.resourcesContainer.state.total_cpu} onChange={this.props.resourcesContainer.handleInputChange}/></label>
                                             		
                                             		<label>total_memory<input type='number' name='total_memory' value={this.props.resourcesContainer.state.total_memory} onChange={this.props.resourcesContainer.handleInputChange}/></label>
-                    					<input type='submit' value='Submit' /> 
+                    					 
+							<button name='sendValue' className={Style.ok} onClick={this.props.resourcesContainer.queryConfirmReservation}>submit2</button>
 						     </form>
 						</div>	
 					     </div>
@@ -51,72 +50,6 @@ export default class Resource extends Component {
 				</section>
 			</section>
 		
-/*
-		<section>
-                <section className={Style2.panel}>
-                    <header>
-                        <div>Resource</div>
-                        <img src='img/ic_close.svg' onClick={()=>this.props.settingContainer.onClose()} />
-                    </header>
-		    <div className={Style2.content}>
-                    <section className={Style.wrap}>
-                        <section className={Style.content}>
-                            <div className={Style.top}>
-                                <img src='img/resource.png' width='80%'/>
-                            </div>
-                            <div className={Style.bottom}>
-                                <div className={Style.left}>
-                                    <div className={Style.time}>{this.props.resourcesContainer.state.description}</div>
-                                    <div className={Style.date}>{this.props.resourcesContainer.state.description}</div>
-                                </div>
-                                <div className={Style.right}>
-                                    <div className={Style.width}>
-                                        <h2>Content</h2>
-                                        <form onSubmit={this.handleSubmit}>
-                                            <lable>site_id:<input type='text' name='site_id' value={this.state.site_id} onChange={this.handleInputChange}/></lable>
-                                            <br/>
-                                            <label>name:<input type='text' name='_name'value={this.state._name} onChange={this.handleInputChange} /></label>
-                                            <br/>
-                                            <label>description:<input type='text' name='description' value={this.state.description} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>contact:<input type='text' name='contact' value={this.state.contact} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>location:<input type='text' name='location' value={this.state.location} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>pragma_boot_path<input type='text' name='pragma_boot_path' value={this.state.pragma_boot_path} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>pragma_boot_version<input type='number' name='pragma_boot_version' value={this.state.pragma_boot_version} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>python_path<input type='text' name='python_path' value={this.state.python_path} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>temp_dir<input type='text' name='temp_dir' value={this.state.temp_dir} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>username<input type='text' name='username' value={this.state.username} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>deployment_type<input type='text' name='deployment_type' value={this.state.deployment_type} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>site_hostname<input type='text' name='site_hostname' value={this.state.site_hostname} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>latitute<input type='text' name='latitude' value={this.state.latitude} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>longtitute<input type='text' name='longitude' value={this.state.longitude} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>total_cpu<input type='number' name='total_cpu' value={this.state.total_cpu} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <label>total_memory<input type='number' name='total_memory' value={this.state.total_memory} onChange={this.handleInputChange}/></label>
-                                            <br/>
-                                            <input type='submit' value='Submit'/> 
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </section>
-		    </div>
-                </section>
-
-            </section>
-*/
 		)
 	}
 
