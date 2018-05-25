9f3b5e9e389158288272ed26f3110e26e1c050ba
visaruth <visaruth.p@gmail.com>
2018-05-25 12:18:43 +0700

    upload for test

---
+++ scripts/test.py
@@ -0,0 +1,17 @@
+#!/opt/python/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 22:17:23 2017
+
+@author: CS401:Nannapas Banluesombatkul
+"""
+
+import cgitb
+cgitb.enable()
+
+
+print "Content-Type: text/html"     
+print "Access-Control-Allow-Origin: *"  
+print
+
+print "hello"
