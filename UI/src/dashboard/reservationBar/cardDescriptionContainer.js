a9522e5580fc638de06bec11e330331a295450bc
Khyojung <918fall@gmail.com>
2019-02-05 12:35:11 -0800

    Error resolution that shows modify button when user is not logged in

--- UI/src/dashboard/reservationBar/cardDescriptionContainer.js
+++ UI/src/dashboard/reservationBar/cardDescriptionContainer.js
@@ -2,7 +2,31 @@ import React,{Component} from 'react'
 import CardDescription from './cardDescription'
 
 export default class cardDescriptionContainer extends Component {
-   render(){
+   componentWillMount(){
+       this.setAdminStatus()
+   }
+   constructor(props){
+        super(props)
+        this.state = {
+            adminStatus : true
+        }
+   }
+   setAdminStatus(){
+       let admin = this.props.app.state.authen.data
+       if(this.props.app.state.authen.isLogedIn){
+           if(admin.status == 'admin' || admin.username == this.props.dashBoardContainer.state.cardDetail.data.admin){
+               this.setState({
+                    adminStatus : false
+                    })
+           }
+       }else{
+               this.setState({
+                    adminStatus : true
+                    })
+       }
+   }
+
+    render(){
         return(
             <section>
                 <CardDescription cardDescriptionContainer = {this}dashBoardContainer={this.props.dashBoardContainer}app={this.props.app}/>
